fff38af5b166c0e159f011b972357fc96c9ec2fb
GGBT <tmddud333@naver.com>
2018-05-19 21:56:50 +0900

    Please someone tell me what is the problem.

--- src/main/java/edu/handong/csee/java/HW3/DataReader.java
+++ src/main/java/edu/handong/csee/java/HW3/DataReader.java
@@ -1,6 +1,8 @@
 package edu.handong.csee.java.HW3;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DataReader {
 	/* GetData to call all the inputs */
@@ -17,7 +19,7 @@ public class DataReader {
 		/* 2. file type Array to contain file names and directory*/
 		File[] files = getListOfFilesFromDirectory(myDirectory);
 
-		/* 3.  */
+		/* 3. String Arraylist to save all the chat data */
 		ArrayList<String> message = readFiles(files);
 
 		System.out.println("hello" + message);
@@ -27,14 +29,13 @@ public class DataReader {
 	private File getDirectory(String Directory) {
 
 		File myDirectory = new File(Directory);
-
 		return myDirectory;
 	}
 
 	private File[] getListOfFilesFromDirectory(File dataDir) {
 
 		for(File file:dataDir.listFiles()) {
-			System.out.println(file.getAbsolutePath());
+			//System.out.println(file.getAbsolutePath());
 		}
 		return dataDir.listFiles();
 	}
@@ -42,10 +43,26 @@ public class DataReader {
 	private ArrayList<String> readFiles(File[] dataDir){
 
 		ArrayList<String> message = new ArrayList<String>();
-
-		// some logics that read files must be here
 		
-
+		for(int i=0;i<dataDir.length;i++)
+		{
+			String fileName = dataDir[i].getAbsolutePath();
+			System.out.println (fileName+" contains : ");
+			Scanner inputStream = null;
+			try {
+				inputStream = new Scanner(new File(fileName));
+			}  catch (FileNotFoundException e) {
+				System.out.println ("Error opening the file " + fileName);
+				System.exit (0);
+			}
+			
+			while (inputStream.hasNextLine()) {
+				String line = inputStream.nextLine ();
+				System.out.println (line);
+			}
+			inputStream.close ();
+		}
+		
 		return message;
 	}
 
